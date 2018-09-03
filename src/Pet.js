dce1b84e6a00ff94e72745f13a9f1e67642b82d9
Dakota Castleberg <dakota.castleberg18@gmail.com>
2018-09-02 23:19:18 -0500

    petfinder added, data being retreived and displayed. eslint plugins added

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
 //react element : dom element, {attributes/props as key/value pairs}, child/ren
 const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, props.name),
+  //     React.createElement("h2", {}, props.animal),
+  //     React.createElement("h2", {}, props.breed)
+  //   ]);
+
+  return (
+    // this is using jsx. litteral translation to above example.
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;
--- src/app.js
+++ src/app.js
@@ -1,36 +1,68 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked the title");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Minneapolis, MN" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets //pets: pets <= javascript trick; same name
+        });
+      });
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "Mixed"
-      })
-    ]);
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
 // Render what(instance of an class), where
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
